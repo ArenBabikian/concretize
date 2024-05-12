03b6fc6dada3b819748ed43bd180736245e99318
JryHL <jerryhl2001@gmail.com>
2024-05-12 15:05:21 -0400

    Ensured independence from working directory; used float for attributes

--- src/language/parser.py
+++ src/language/parser.py
@@ -1,10 +1,13 @@
 from textx import metamodel_from_file
+import pathlib
 
-metamodel = metamodel_from_file('grammar.tx')
+baseFile = pathlib.Path(__file__).parent.resolve()
 
-model = metamodel.model_from_file('example.concretize')
+metamodel = metamodel_from_file(f'{baseFile}/grammar.tx')
+
+model = metamodel.model_from_file(f'{baseFile}/example.concretize')
 # Also possible to just take a string:
 # model = metamodel.model_from_str(str)
 
 for o in model.objects:
-	print(type(o).__name__, o.name)
+	print(f"{type(o).__name__}: {o.__dict__}\n")
