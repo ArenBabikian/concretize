6a9489b317066c3d5b98d205a19d64e6eaa0a766
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2024-05-15 12:27:29 -0400

    Refine grammar for constraints

--- src/language/parser.py
+++ src/language/parser.py
@@ -9,5 +9,7 @@ model = metamodel.model_from_file(f'{baseFile}/example.concretize')
 # Also possible to just take a string:
 # model = metamodel.model_from_str(str)
 
-for o in model.objects:
+for o in model.actors:
+	print(f"{type(o).__name__}: {o.__dict__}\n")
+for o in model.constraints:
 	print(f"{type(o).__name__}: {o.__dict__}\n")
