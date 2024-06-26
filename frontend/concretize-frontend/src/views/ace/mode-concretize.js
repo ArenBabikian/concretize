d43b9f3c35f42f3663a79956a94b2db53ec70040
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2024-06-26 15:39:37 +0800

    Remove commented-out highlighting rules

--- frontend/concretize-frontend/src/views/ace/mode-concretize.js
+++ frontend/concretize-frontend/src/views/ace/mode-concretize.js
@@ -25,63 +25,7 @@ var DiffHighlightRules = function () {
                     "support.function", // Constraint name, before parenthesis
                     "text" // Parenthesis
                 ]
-            },
-            // {
-            //     regex: "^(?:\\*{15}|={67}|-{3}|\\+{3})$",
-            //     token: "punctuation.definition.separator.diff",
-            //     "name": "keyword"
-            // }, {
-            //     regex: "^(@@)(\\s*.+?\\s*)(@@)(.*)$",
-            //     token: [
-            //         "constant",
-            //         "constant.numeric",
-            //         "constant",
-            //         "comment.doc.tag"
-            //     ]
-            // }, {
-            //     regex: "^(\\d+)([,\\d]+)(a|d|c)(\\d+)([,\\d]+)(.*)$",
-            //     token: [
-            //         "constant.numeric",
-            //         "punctuation.definition.range.diff",
-            //         "constant.function",
-            //         "constant.numeric",
-            //         "punctuation.definition.range.diff",
-            //         "invalid"
-            //     ],
-            //     "name": "meta."
-            // }, {
-            //     regex: "^(\\-{3}|\\+{3}|\\*{3})( .+)$",
-            //     token: [
-            //         "constant.numeric",
-            //         "meta.tag"
-            //     ]
-            // }, {
-            //     regex: "^([!+>])(.*?)(\\s*)$",
-            //     token: [
-            //         "support.constant",
-            //         "text",
-            //         "invalid"
-            //     ]
-            // }, {
-            //     regex: "^([<\\-])(.*?)(\\s*)$",
-            //     token: [
-            //         "support.function",
-            //         "string",
-            //         "invalid"
-            //     ]
-            // }, {
-            //     regex: "^(diff)(\\s+--\\w+)?(.+?)( .+)?$",
-            //     token: ["variable", "variable", "keyword", "variable"]
-            // }, {
-            //     regex: "^Index.+$",
-            //     token: "variable"
-            // }, {
-            //     regex: "^\\s+$",
-            //     token: "text"
-            // }, {
-            //     regex: "\\s*$",
-            //     token: "invalid"
-            // }
+            }
         ]
     };
 };
