f24fd95db28adf805cbfbc57a1e657487576b3c4
ArenBabikian <aren.babikian@mail.mcgill.ca>
2025-07-30 02:48:37 -0400

    scenic: Implement output of unified times file for f2l

--- evaluation/SOSYM25/scenic/scenic-evaluation.py
+++ evaluation/SOSYM25/scenic/scenic-evaluation.py
@@ -1,3 +1,4 @@
+import json
 import os
 import scenic
 import time
@@ -5,17 +6,26 @@ import csv
 
 class ScenicEval():
     
-    def __init__(self, scenario_file, map, intersection, required_unique_scenes, timeout):
+    def __init__(self, scenario_file, map, intersection, actors, required_unique_scenes, timeout):
         self.scenario_file = scenario_file
         self.map = map
         self.intersection = intersection
+        self.actors = actors
         self.required_unique_scenes = required_unique_scenes
         self.timeout = timeout
         self.generated_scenes = []
         self.unique_scenes = set()
+        self.total_runtime = -1
+
+    def all_scenarios_found(self):
+        return len(self.unique_scenes) >= self.required_unique_scenes
+
+    def timeout_reached(self):
+        return (time.time() - self.start_time) >= self.timeout
     
     def should_run(self):
-        return len(self.unique_scenes) < self.required_unique_scenes and (time.time() - self.start_time) < self.timeout
+        # return (time.time() - self.start_time) < self.timeout
+        return not self.all_scenarios_found() and not self.timeout_reached()
 
     def extract_trajectory(vehicle):
         return tuple(lane.uid for lane in vehicle.behavior._kwargs['trajectory'])
@@ -45,6 +55,12 @@ class ScenicEval():
             new_size = len(self.unique_scenes)
             if new_size > prev_size:
                 self.generated_scenes.append((scene, generation_time, num_iterations))
+
+        if self.timeout_reached():
+            self.total_runtime = self.timeout
+        else:
+            self.total_runtime = time.time() - self.start_time
+        
         return self.generated_scenes, self.unique_scenes
 
     def save_generation_time_to_file(self, path):
@@ -75,15 +91,50 @@ class ScenicEval():
                         lanes.append(lane)
                 writer.writerow(lanes)
 
+    def save_times(self, output_file_path):
+        # Load or initialize output JSON
+        if os.path.exists(output_file_path):
+            with open(output_file_path, 'r') as outfile:
+                try:
+                    output_data = json.load(outfile)
+                except json.JSONDecodeError:
+                    output_data = {}
+        else:
+            output_data = {}
+
+        j_id = str(self.intersection)
+        n_ac = str(self.actors)
+
+        # Ensure nested structure
+        if j_id not in output_data:
+            output_data[j_id] = {}
+        if n_ac not in output_data[j_id]:
+            output_data[j_id][n_ac] = {}
+
+        # Update the output data with the timing values
+        output_data[j_id][n_ac]['f2l-time'] = self.total_runtime
+        output_data[j_id][n_ac]['f2l-required-unique-scenes'] = self.required_unique_scenes
+        output_data[j_id][n_ac]['f2l-generated-scenes'] = len(self.generated_scenes)
+        output_data[j_id][n_ac]['f2l-success-rate'] = len(self.unique_scenes) / self.required_unique_scenes  if self.required_unique_scenes > 0 else -1.0
+        # output_data[j_id][n_ac]['l2c-time-median'] = l2c_median
+        # output_data[j_id][n_ac]['l2c-success-rate'] = 1.0
+
+        # Write back to output file
+        with open(output_file_path, 'w') as outfile:
+            json.dump(output_data, outfile, indent=2)
+
 base_dir = 'evaluation/SOSYM25/scenic'
 scenario_file_path = base_dir + '/scenic-maneuvers{actors}.scenic'
-timeout = 5 # seconds
+timeout = 20 # seconds
+
+all_times_path = f'{base_dir}/output/f2l-times.json'
 
 configs = [('Town04', 916, 1, 12), ('Town04', 916, 2, 56), ('Town04', 916, 3, 124), ('Town04', 916, 4, 160), 
            ('Town05', 2240, 1, 8), ('Town05', 2240, 2, 14), ('Town05', 2240, 3, 13), ('Town05', 2240, 4, 6)]
 for map, intersection, actors, required in configs:
-    eval = ScenicEval(scenario_file=scenario_file_path.format(actors=actors), map=map, intersection=intersection, required_unique_scenes=required, timeout=timeout)
+    eval = ScenicEval(scenario_file=scenario_file_path.format(actors=actors), map=map, intersection=intersection, actors=actors, required_unique_scenes=required, timeout=timeout)
     scenes, unique_scenes = eval.generate()
     eval.save_logical_scenarios_to_file(f"{base_dir}/logical-scenarios/{map}_{intersection}_{actors}ac.csv")
     eval.save_generation_time_to_file(f"{base_dir}/output/generation_time_{map}_{intersection}_{actors}ac.csv")
+    eval.save_times(all_times_path)
     print("With ", actors, "actors, out of ", required, " possible unique scenes we found ", len(unique_scenes), " with a total of ", len(scenes), " scenes generated")
