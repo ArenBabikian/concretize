2bc89c1137046b023f86389bd6dfbe30fce1d363
Attila Ficsor <ficsorattila96@gmail.com>
2025-08-01 18:15:59 +0200

    Scenic: add L2C to statistics calculation

--- evaluation/SOSYM25/statistics/statistics_scenic.py
+++ evaluation/SOSYM25/statistics/statistics_scenic.py
@@ -1,29 +1,48 @@
 import os
 import pandas as pd
 
+# F2L
 # Input
 base_dir = 'evaluation/SOSYM25'
-stats_file_path = f'{base_dir}/all_output/scenic/f2l/generation_times.csv'
+f2l_stats_file_path = f'{base_dir}/all_output/scenic/f2l/generation_times.csv'
 
 # output paths
 output_folder_path = f'{base_dir}/statistics/output/scenic'
 os.makedirs(output_folder_path, exist_ok=True)
-aggregate_stats_file_path = f'{output_folder_path}/f2l_median_generation_times.csv'
-total_stats_file_path = f'{output_folder_path}/f2l_total_generation_times.csv'
+f2l_aggregate_stats_file_path = f'{output_folder_path}/f2l_median_generation_times.csv'
+f2l_total_stats_file_path = f'{output_folder_path}/f2l_total_generation_times.csv'
 
-df = pd.read_csv(stats_file_path)
+df_f2l = pd.read_csv(f2l_stats_file_path)
 
 # Get total times and save
-df['max_scene'] = df.groupby(['map', 'junction', 'run_id', 'actors'])['scene_number'].transform('max')
-df_total_times = df[df['scene_number'] == df['max_scene']]
-df_total_times.to_csv(total_stats_file_path)
+df_f2l['max_scene'] = df_f2l.groupby(['map', 'junction', 'run_id', 'actors'])['scene_number'].transform('max')
+df_f2l_total_times = df_f2l[df_f2l['scene_number'] == df_f2l['max_scene']]
+df_f2l_total_times.to_csv(f2l_total_stats_file_path)
 
 # Group total times, save aggregated
 # result = df_total_times.groupby(['map', 'junction', 'actors'])['time'].median().reset_index().rename(columns={'time': 'median_time'})
-result = df_total_times.groupby(['map', 'junction', 'actors']).agg(
+result_f2l = df_f2l_total_times.groupby(['map', 'junction', 'actors']).agg(
     median_time=('time', 'median'),
     num_runs=('time', 'size'),
     max_scene_number=('scene_number', 'max')
 ).reset_index()
-result.to_csv(aggregate_stats_file_path)
-result.to_latex(aggregate_stats_file_path + ".txt")
+result_f2l.to_csv(f2l_aggregate_stats_file_path)
+result_f2l.to_latex(f2l_aggregate_stats_file_path + ".tex")
+
+
+# L2C
+# Input
+l2c_stats_file_path = f'{base_dir}/all_output/scenic/l2c/concretization_times.csv'
+
+# Output
+l2c_aggregate_stats_file_path = f'{output_folder_path}/l2c_median_concretization_times.csv'
+
+df_l2c = pd.read_csv(l2c_stats_file_path)
+
+# Calculate success rate and runtime 
+result_l2c = df_l2c.groupby(['map', 'junction', 'actors']).agg(
+    success_ratio=('success', 'mean'),
+    median_runtime=('runtime', lambda x: x[df_l2c.loc[x.index, 'success']].median())
+).reset_index()
+result_l2c.to_csv(l2c_aggregate_stats_file_path)
+result_l2c.to_latex(l2c_aggregate_stats_file_path + ".tex")
