aad5ea4211b69328f87fd07db916024f3d561450
ArenBabikian <aren.babikian@mail.mcgill.ca>
2025-07-31 21:15:08 -0400

    Minor adjsutment to f2l-scenic statistics output

--- evaluation/SOSYM25/scenic/statistics.py
+++ evaluation/SOSYM25/scenic/statistics.py
@@ -1,12 +1,27 @@
 import pandas as pd
 
-base_dir = 'evaluation/SOSYM25/scenic'
-stats_file_path = f'{base_dir}/output/generation_times.csv'
-aggregate_stats_file_path = f'{base_dir}/output/median_generation_times.csv'
+# Input
+base_dir = 'evaluation/SOSYM25'
+stats_file_path = f'{base_dir}/scenic/output/generation_times.csv'
+
+# output paths
+output_folder_path = f'{base_dir}/statistics/output/scenic'
+aggregate_stats_file_path = f'{output_folder_path}/median_generation_times.csv'
+total_stats_file_path = f'{output_folder_path}/total_generation_times.csv'
 
 df = pd.read_csv(stats_file_path)
+
+# Get total times and save
 df['max_scene'] = df.groupby(['map', 'junction', 'run_id', 'actors'])['scene_number'].transform('max')
 df_total_times = df[df['scene_number'] == df['max_scene']]
-result = df_total_times.groupby(['map', 'junction', 'actors'])['time'].median().reset_index().rename(columns={'time': 'median_time'})
+df_total_times.to_csv(total_stats_file_path)
+
+# Group total times, save aggregated
+# result = df_total_times.groupby(['map', 'junction', 'actors'])['time'].median().reset_index().rename(columns={'time': 'median_time'})
+result = df_total_times.groupby(['map', 'junction', 'actors']).agg(
+    median_time=('time', 'median'),
+    num_runs=('time', 'size'),
+    max_scene_number=('scene_number', 'max')
+).reset_index()
 result.to_csv(aggregate_stats_file_path)
 result.to_latex(aggregate_stats_file_path + ".txt")
